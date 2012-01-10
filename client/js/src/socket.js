9c7a9842753c2a4a6627abd5a18bc043a1689ef3
adam j. sontag <ajpiano@ajpiano.com>
2012-01-09 22:34:45 -0500

    allow client to init when socket is not able to connect

--- client/js/src/socket.js
+++ client/js/src/socket.js
@@ -1,3 +1,6 @@
 define([],function() {
-  return io.connect("ws://localhost:1337");
+  return window.io ? io.connect("ws://localhost:1337") : {
+    on: function() {},
+    off: function() {}
+  };
 });
